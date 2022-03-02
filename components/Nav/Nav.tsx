7e34621249f5040f368ae821d2a691fff3995a76
Alvin Leung <alvinleung2009@gmail.com>
2022-03-01 22:55:21 -0800

    Nav havestsate

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Button from "../Button/Button";
 import { motion } from "framer-motion";
 import { AnimationConfig } from "../AnimationConfig";
@@ -7,13 +7,23 @@ type Props = {
   children: React.ReactNode;
 };
 
-const NavLink = ({ children, href, isActive, navItemIndex, currentIndex }) => {
+const NavLink = ({
+  children,
+  href,
+  isActive,
+  navItemIndex,
+  currentIndex,
+  onLinkHover,
+  onClick,
+}) => {
   const motionDiretion = currentIndex > navItemIndex ? "+" : "-";
 
   return (
     <a
       href={href}
       className="text-tiny no-underline mr-8 text-black flex flex-row items-center"
+      onMouseEnter={() => onLinkHover()}
+      onClick={onClick}
     >
       <div className="rounded-full w-[.5em] h-[.5em] border border-black mr-[.3em] overflow-hidden">
         <motion.div
@@ -43,24 +53,63 @@ const NavContext = React.createContext<INavContext>(null);
 export const useNavContext = () => useContext(NavContext);
 
 const Nav = ({ children }: Props) => {
+  // the section in view
   const [currentSection, setCurrentSection] = useState(NavSections[0]);
-  const selectionIndex = NavSections.indexOf(currentSection);
+  // the dispalying section
+  const [activeSection, setActiveSection] = useState(null);
+  const selectionIndex = NavSections.indexOf(activeSection);
+
+  const [isJumpingToSection, setIsJumpingToSection] = useState(false);
+
+  const jumpSectionTimer = useRef(null);
+  function jumpToSection() {
+    setIsJumpingToSection(true);
+    jumpSectionTimer.current && clearTimeout(jumpSectionTimer.current);
+    jumpSectionTimer.current = setTimeout(
+      () => setIsJumpingToSection(false),
+      1000
+    );
+  }
+  useEffect(() => {
+    const handleWheel = () => {
+      setIsJumpingToSection(false);
+      jumpSectionTimer.current && clearTimeout(jumpSectionTimer.current);
+    };
+    window.addEventListener("wheel", () => handleWheel);
+    return () => {
+      window.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isJumpingToSection) return;
+    setActiveSection(currentSection);
+  }, [currentSection, isJumpingToSection]);
 
   return (
     <>
       <nav className="mx-4 lg:mx-document-side z-[1000] h-24 fixed left-0 right-0 flex flex-row items-center align-center">
-        <div className="flex flex-row">
-          {NavSections.map((navSection, i) => (
-            <NavLink
-              href={`#${navSection}`}
-              isActive={currentSection === navSection}
-              currentIndex={selectionIndex}
-              navItemIndex={i}
-              key={i}
-            >
-              {navSection}
-            </NavLink>
-          ))}
+        <div
+          className="flex flex-row"
+          onMouseLeave={() => setActiveSection(currentSection)}
+        >
+          {NavSections.map((navSection, i) => {
+            const isHoveringSection = activeSection === navSection;
+
+            return (
+              <NavLink
+                href={`#${navSection}`}
+                isActive={isHoveringSection}
+                currentIndex={selectionIndex}
+                navItemIndex={i}
+                key={i}
+                onLinkHover={() => setActiveSection(navSection)}
+                onClick={() => jumpToSection()}
+              >
+                {navSection}
+              </NavLink>
+            );
+          })}
         </div>
         <div className="ml-auto text-[30px]">
           <Button noDescender>Get Tickets</Button>
