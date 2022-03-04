f45e5d363177af6299712e08efbfb6dc553fe5aa
Alvin Leung <alvinleung2009@gmail.com>
2022-03-04 15:40:26 -0800

    scrolling bottom bar

--- components/InfoBar/InfoBar.tsx
+++ components/InfoBar/InfoBar.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import { AnimationConfig } from "../AnimationConfig";
+import useMeasureElement from "../../hooks/useMeasureElement";
 
 type Props = { children: React.ReactNode };
 const InfoBar = ({ children }: Props) => {
+  const [measurement, ref] = useMeasureElement<HTMLDivElement>([]);
+  const [containerMeasurement, containerRef] =
+    useMeasureElement<HTMLDivElement>([]);
+
   return (
     <motion.div
       className="fixed bottom-0 text-tiny bg-accent-red border-t border-black w-full overflow-hidden z-40"
@@ -15,14 +20,21 @@ const InfoBar = ({ children }: Props) => {
           ease: AnimationConfig.EASING,
         },
       }}
+      ref={containerRef}
     >
       <motion.div
         className="my-1 whitespace-nowrap"
-        initial={{ x: 1000 }}
+        initial={{ x: "100%" }}
         animate={{
-          x: -1000,
-          transition: { duration: 20, ease: "linear", repeat: Infinity },
+          x: measurement && -measurement.width,
+          transition: {
+            duration:
+              containerMeasurement && containerMeasurement.width * 0.014,
+            ease: "linear",
+            repeat: Infinity,
+          },
         }}
+        ref={ref}
       >
         {children}
       </motion.div>
